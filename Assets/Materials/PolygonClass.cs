ad9d3e5bfdf913656f969c1afbdd2d7c13115947
xhriman <seal789ie@gmail.com>
2015-03-17 00:59:23 +0800

    polygon class

--- Assets/Materials/PolygonClass.cs
+++ Assets/Materials/PolygonClass.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class PolygonClass : MonoBehaviour {
+public class Polygon
+{
+    private int numOfVertices;
+    private List<Vector2> Vertices = new List<Vector2>();
+    public int NumVertices { get { return numOfVertices; } }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    Polygon()
+    {
+        numOfVertices = 3;
+        for (int i = 0; i < numOfVertices; i++)
+            Vertices.Add(new Vector2());
+    }
+
+    //Polygon(int nVertice)
+    //{
+    //    numOfVertices = nVertice;
+    //    for (int i = 0; i < numOfVertices; i++)
+    //        Vertices.Add(new UnityEngine.Vector2());
+    //}
+
+    Polygon(int nVertice,Vector2[] vertex)
+    {
+        numOfVertices = nVertice;
+        for (int i = 0; i < numOfVertices; i++)
+            Vertices.Add(vertex[i]);
+    }
+
+    Polygon(int nVertice, float[][] vertex)
+    {
+        numOfVertices = nVertice;
+        for (int i = 0; i < numOfVertices; i++)
+            Vertices.Add(new Vector2(vertex[i][0], vertex[i][1]));
+    }
+
+    public Vector2 getVertex(int index)
+    {
+        return Vertices[index];
+    }
+
+    public void addVertex(Vector2 newVertex)
+    {
+        ++numOfVertices;
+        Vertices.Add(newVertex);
+    }
+
+    public void modifyVertex(int index,Vector2 newVertex)
+    {
+        Vertices[index] = newVertex;
+    }
 }
